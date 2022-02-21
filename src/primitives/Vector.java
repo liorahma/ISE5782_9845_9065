f543a2d6103db27c591b83a289d956847671fd0e
liora <liora@DESKTOP-46I6K5L>
2022-02-21 14:47:24 +0200

    add

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,10 +1,15 @@
 package primitives;
 
-public class Vector extends Point{
+public class Vector extends Point {
     public Vector(double x, double y, double z) {
         super(x, y, z);
-        if (Double3.ZERO.equals(new Double3(x, y, z))){
+        if (Double3.ZERO.equals(new Double3(x, y, z))) {
             throw new IllegalArgumentException("ZERO vector is not allowed");
         }
+    }
+
+    public Vector add(Vector other) {
+        Double3 result = super.add(other).xyz;
+        return new Vector(xyz.d1, xyz.d2, xyz.d3);
     }
 }
