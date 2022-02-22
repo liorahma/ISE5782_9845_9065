9374eba1d57c9614ff3ef0913c17a6a0a30459b9
liora <liora@DESKTOP-46I6K5L>
2022-02-22 15:50:01 +0200

    documentation

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -3,8 +3,17 @@ package primitives;
 import java.util.Objects;
 
 public class Point {
+    /**
+     * 3D representation of the point
+     */
     protected Double3 xyz;
 
+    /**
+     * Builds representation of a 3D point
+     * @param x x-axis value
+     * @param y y-axis value
+     * @param z z-axis value
+     */
     public Point(double x, double y, double z) {
         xyz = new Double3(x, y, z);
     }
@@ -17,10 +26,11 @@ public class Point {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Point)) return false;
+        Point point = (Point) obj;
         return Objects.equals(xyz, point.xyz);
     }
 
@@ -29,16 +39,47 @@ public class Point {
         return Objects.hash(xyz);
     }
 
+    /**
+     * Adds a vector to a point
+     * @param vector the vector to add
+     * @return the end point of the vector starting from the point
+     */
     public Point add(Vector vector) {
         Double3 newXyz = vector.xyz.add(this.xyz);
         return new Point(newXyz.d1, newXyz.d2, newXyz.d3);
     }
 
+    /**
+     * Subtracts a given point from the current point
+     * @param point the point to subtract
+     * @return the resulting vector
+     */
     public Vector subtract(Point point) {
         Double3 newXyz = point.xyz.subtract(this.xyz);
         if (Double3.ZERO.equals(newXyz)) {
-            throw new IllegalArgumentException("subtraction resulting ZERO vector - not allowed");
+            throw new IllegalArgumentException("subtraction resulting with ZERO vector - not allowed");
         }
         return new Vector(newXyz.d1, newXyz.d2, newXyz.d3);
+    }
+
+    /**
+     * Calculates the squared value of the distance between 2 points
+     * @param point the point to calculate distance from
+     * @return the distance squared
+     */
+    public double distanceSquared(Point point) {
+        double deltaX = this.xyz.d1 - point.xyz.d1;
+        double deltaY = this.xyz.d2 - point.xyz.d2;
+        double deltaZ = this.xyz.d3 - point.xyz.d3;
+        return deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ;
+    }
+
+    /**
+     * Calculates the value of the distance between 2 points
+     * @param point the point to calculate distance from
+     * @return the distance
+     */
+    public double distance(Point point) {
+        return Math.sqrt(this.distanceSquared(point));
     }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,6 +1,12 @@
 package primitives;
 
 public class Vector extends Point {
+    /**
+     * Builds representation of a 3D vector
+     * @param x x-axis value
+     * @param y y-axis value
+     * @param z z-axis value
+     */
     public Vector(double x, double y, double z) {
         super(x, y, z);
         if (Double3.ZERO.equals(new Double3(x, y, z))) {
@@ -8,8 +14,47 @@ public class Vector extends Point {
         }
     }
 
-    public Vector add(Vector other) {
-        Double3 result = super.add(other).xyz;
-        return new Vector(xyz.d1, xyz.d2, xyz.d3);
+    @Override
+    public String toString() {
+        return "Vector{" + super.toString()
+                + "}";
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    /**
+     * Adds a vector to a vector
+     * @param vector the vector to add
+     * @return the resulting
+     */
+    @Override
+    public Vector add(Vector vector) {
+        Double3 newXyz = new Double3(vector.xyz.d1 + this.xyz.d1,
+                vector.xyz.d2 + this.xyz.d2,
+                vector.xyz.d3 + this.xyz.d3);
+        if (Double3.ZERO.equals(newXyz)) {
+            throw new IllegalArgumentException("addition resulting with ZERO vector - not allowed");
+        }
+        return new Vector(newXyz.d1, newXyz.d2, newXyz.d3);
+    }
+
+    /**
+     * Subtracts a given vector from the current vector
+     * @param vector the vector to subtract
+     * @return the resulting vector
+     */
+    public Vector subtract(Vector vector) {
+        Double3 newXyz = new Double3(vector.xyz.d1 - this.xyz.d1,
+                vector.xyz.d2 - this.xyz.d2,
+                vector.xyz.d3 - this.xyz.d3);
+        if (Double3.ZERO.equals(newXyz)) {
+            throw new IllegalArgumentException("subtraction resulting with ZERO vector - not allowed");
+        }
+        return new Vector(newXyz.d1, newXyz.d2, newXyz.d3);
+    }
+
+
 }
