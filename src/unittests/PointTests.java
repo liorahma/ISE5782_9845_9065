8871a838fb2df01d8b5b09927142ca29491ef096
liora <liora@DESKTOP-46I6K5L>
2022-03-07 10:31:53 +0200

    unittests

---
+++ src/unittests/PointTests.java
@@ -0,0 +1,31 @@
+package unittests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for primitives.Point class
+ * @author Liorah Mandelbaum and Sarah Bednarsh
+ */
+class PointTests {
+
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Vector)}.
+     */
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void distanceSquared() {
+    }
+
+    @Test
+    void distance() {
+    }
+}
---
+++ src/unittests/VectorTests.java
@@ -0,0 +1,45 @@
+package unittests;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for primitives.Vector class
+ * @author Liorah Mandelbaum and Sarah Bednarsh
+ */
+class VectorTests {
+
+    /**
+     * Test method for {@link primitives.Vector#add(primitives.Vector)}.
+     */
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void normalize() {
+    }
+
+    @Test
+    void length() {
+    }
+
+    @Test
+    void lengthSquared() {
+    }
+
+    @Test
+    void crossProduct() {
+    }
+
+    @Test
+    void dotProduct() {
+    }
+
+    @Test
+    void scale() {
+    }
+}
