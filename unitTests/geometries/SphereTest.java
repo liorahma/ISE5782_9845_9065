91da63a2f4dbb852d992f5fad131c0b76af2678c
SarahBednarsh <sarahbednarsh@gmail.com>
2022-03-08 18:57:16 +0200

    Sphere and triangle

--- unitTests/geometries/SphereTest.java
+++ unitTests/geometries/SphereTest.java
@@ -1,13 +1,19 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SphereTest {
-
+    /**
+     * Test method for {@link Sphere#getNormal(Point)}
+     */
     @Test
-    void getNormal() {
+    void testGetNormal() {
         // ============ Equivalence Partitions Tests ==============
+        Sphere s= new Sphere(new Point(0,0,0),2d);
+        Point p = new Point(0,0,2);
+        Vector v= p.subtract(s.getCenter());
     }
 }
