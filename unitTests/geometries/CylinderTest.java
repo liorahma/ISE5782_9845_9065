1e60f831b6ad49717789aa4d45a8ca6e12db7bf0
liora <liora@DESKTOP-46I6K5L>
2022-03-10 14:36:26 +0200

    test normals for tube and cylinder. Tube: 1 EP and 1 BVA Cylinder: 3 EP

--- unitTests/geometries/CylinderTest.java
+++ unitTests/geometries/CylinderTest.java
@@ -1,7 +1,7 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
-import primitives.Point;
+import primitives.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +12,35 @@ class CylinderTest {
      */
     @Test
     void testGetNormal() {
-        
+        // ============ Equivalence Partitions Tests ==============
+        Cylinder c = new Cylinder(new Ray(new Point(0, 0, 0), new Vector(0, 0, 1)),
+                2d, 2d);
+
+        // On the side of cylinder (tube)
+        Point pointOnSide = new Point(1, 0, 1);
+        Vector normalOption1 = new Vector(1, 0, 0);
+        Vector normalOption2 = new Vector(-1, 0, 0);
+        Vector sideNormal = c.getNormal(pointOnSide);
+        assertTrue(
+                sideNormal.equals(normalOption1) || sideNormal.equals(normalOption2),
+                "getNormal() of cylinder doesn't work correctly for point on the side"
+        );
+
+        Vector normalForBase = new Vector(0, 0, 1);
+        // On first base - distant
+        Point pointOnBase1 = new Point(1, 0, 2);
+        assertEquals(
+                normalForBase,
+                c.getNormal(pointOnBase1),
+                "getNormal() of cylinder doesn't work correctly for point on distant base"
+        );
+
+        // On second base - the on with p0 on it
+        Point pointOnBase2 = new Point(1, 0, 0);
+        assertEquals(
+                normalForBase,
+                c.getNormal(pointOnBase2),
+                "getNormal() of cylinder doesn't work correctly for point on base with p0"
+        );
     }
 }
--- unitTests/geometries/TubeTest.java
+++ unitTests/geometries/TubeTest.java
@@ -1,12 +1,38 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TubeTest {
 
+    /**
+     * Test method {@link Tube#getNormal(Point)}
+     */
     @Test
     void testGetNormal() {
+        Tube tube = new Tube(new Ray(new Point(0, 0, 0), new Vector(0, 0, 1)), 2d);
+        // ============ Equivalence Partitions Tests ==============
+
+        Point point = new Point(1, 0, 2);
+        Vector normalOption1 = new Vector(1, 0, 0);
+        Vector normalOption2 = new Vector(-1, 0, 0);
+        Vector tubeNormal = tube.getNormal(point);
+        assertTrue(
+                tubeNormal.equals(normalOption1) || tubeNormal.equals(normalOption2),
+                "getNormal() for sphere doesn't work correctly"
+        );
+
+        // =============== Boundary Values Tests ==================
+        // When vector resulting from subtraction of point and p0 is orthogonal to axisRay vector
+        Point boundaryPoint = new Point(1, 0, 0);
+        Vector boundaryTubeNormal = tube.getNormal(boundaryPoint);
+        assertTrue(
+                boundaryTubeNormal.equals(normalOption1) || boundaryTubeNormal.equals(normalOption2),
+                "getNormal() for sphere doesn't work correctly for boundary value"
+        );
     }
 }
