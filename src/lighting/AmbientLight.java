7fcbd6fd81ac36681fc61da0e5fca5d2263517ed
SarahBednarsh <sarahbednarsh@gmail.com>
2022-04-26 09:08:45 +0300

    finished shadow test- targil 7.1

--- src/lighting/AmbientLight.java
+++ src/lighting/AmbientLight.java
@@ -18,12 +18,20 @@ public class AmbientLight extends Light {
     }
 
     /**
-     * constructor for ambient light
+     * constructor for ambient light with Double3
      * @param intensity intensity of ambient light
      * @param ka attenuation coefficient
      */
     public AmbientLight(Color intensity, Double3 ka) {
         super(intensity.scale(ka));
+    }
+    /**
+     * constructor for ambient light with double
+     * @param intensity intensity of ambient light
+     * @param ka attenuation coefficient
+     */
+    public AmbientLight(Color intensity, double ka) {
+        super(intensity.scale(new Double3(ka,ka,ka)));
     }
 
 
