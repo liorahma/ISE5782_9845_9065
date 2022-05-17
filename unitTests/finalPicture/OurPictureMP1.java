b64bc6e71a195086bf4b8d8eea97bccd83f3fe6d
SarahBednarsh <sarahbednarsh@gmail.com>
2022-05-17 10:16:33 +0300

    picture updating

--- unitTests/finalPicture/OurPictureMP1.java
+++ unitTests/finalPicture/OurPictureMP1.java
@@ -17,25 +17,43 @@ public class OurPictureMP1 {
     @Test
     public void picture() {
         int lengthToX=20000;
-        int firstX= 3000;
+        int firstX= 6500;
         int distanceBetweenPins=50;
-        Color laneColor= new Color(255, 223, 128);
+        Color laneColor= new Color(255, 223, 128).scale(4d/3d);
         Material bowlMat= new Material().setKs(1).setKd(0.3).setKr(0.3).setShininess(19);
         Camera camera = new Camera(new Point(-1000, 0, 300), new Vector(1, 0, -0.05), new Vector(0.05, 0, 1)) //
                 .setVPSize(200, 150).setVPDistance(1000);
         Camera cameraTop = new Camera(new Point(3000, 0, 1500), new Vector(0, 0, -1), new Vector(1, 0, 0)) //
                 .setVPSize(200, 150).setVPDistance(1000);
+
+
         Polygon lane = (Polygon) new Polygon(new Point(-500, 100, 0), new Point(lengthToX, 100, 0), new Point(lengthToX, -100, 0), new Point(-500, -100, 0)).setEmission(laneColor)
                 .setMaterial(new Material().setKs(1).setKd(0.6).setKr(0.3).setShininess(19));
-        Polygon leftPolR = (Polygon) new Polygon(new Point(-500, 100, 0), new Point(lengthToX, 100, 0), new Point(lengthToX, 120, -20), new Point(-500, 120, -20)).setEmission(new Color(GRAY))
+        //original with two polygons
+//        Polygon leftPolR = (Polygon) new Polygon(new Point(-500, 100, 0), new Point(lengthToX, 100, 0), new Point(lengthToX, 120, -20), new Point(-500, 120, -20)).setEmission(new Color(GRAY))
+//                .setMaterial(new Material().setKs(1).setKd(0.3).setKr(0.3).setShininess(19));
+//        Polygon leftPolL = (Polygon) new Polygon(new Point(-500, 140, 0), new Point(lengthToX, 140, 0), new Point(lengthToX, 120, -20), new Point(-500, 120, -20)).setEmission(new Color(GRAY))
+//                .setMaterial(new Material().setKs(1).setKd(0.3).setKr(0.3).setShininess(19));
+        //three- a little meshy
+
+        Polygon leftPolR = (Polygon) new Polygon(new Point(-500, 100, 0), new Point(lengthToX, 100, 0), new Point(lengthToX, 110, -20), new Point(-500, 110, -20)).setEmission(new Color(GRAY))
                 .setMaterial(new Material().setKs(1).setKd(0.3).setKr(0.3).setShininess(19));
-        Polygon leftPolL = (Polygon) new Polygon(new Point(-500, 140, 0), new Point(lengthToX, 140, 0), new Point(lengthToX, 120, -20), new Point(-500, 120, -20)).setEmission(new Color(GRAY))
+        Polygon leftPolM = (Polygon) new Polygon(new Point(-500, 110, -20), new Point(lengthToX, 110, -20), new Point(lengthToX, 130, -20), new Point(-500, 130, -20)).setEmission(new Color(GRAY))
                 .setMaterial(new Material().setKs(1).setKd(0.3).setKr(0.3).setShininess(19));
-        Polygon rightPolR = (Polygon) new Polygon(new Point(-500, -140, 0), new Point(lengthToX, -140, 0), new Point(lengthToX, -120, -20), new Point(-500, -120, -20)).setEmission(new Color(GRAY))
+        _scene._geometries.add(leftPolM);
+        Polygon leftPolL = (Polygon) new Polygon(new Point(-500, 140, 0), new Point(lengthToX, 140, 0), new Point(lengthToX, 130, -20), new Point(-500, 130, -20)).setEmission(new Color(GRAY))
                 .setMaterial(new Material().setKs(1).setKd(0.3).setKr(0.3).setShininess(19));
-        Polygon rightPolL = (Polygon) new Polygon(new Point(-500, -100, 0), new Point(lengthToX, -100, 0), new Point(lengthToX, -120, -20), new Point(-500, -120, -20)).setEmission(new Color(GRAY))
+
+
+
+        Polygon rightPolR = (Polygon) new Polygon(new Point(-500, -140, 0), new Point(lengthToX, -140, 0), new Point(lengthToX, -130, -20), new Point(-500, -130, -20)).setEmission(new Color(GRAY))
                 .setMaterial(new Material().setKs(1).setKd(0.3).setKr(0.3).setShininess(19));
-        Color bowlingMain= new Color(WHITE).scale(10);
+        Polygon rightPolM = (Polygon) new Polygon(new Point(-500, -110, -20), new Point(lengthToX, -110, -20), new Point(lengthToX, -130, -20), new Point(-500, -130, -20)).setEmission(new Color(GRAY))
+                .setMaterial(new Material().setKs(1).setKd(0.3).setKr(0.3).setShininess(19));
+        _scene._geometries.add(rightPolM);
+        Polygon rightPolL = (Polygon) new Polygon(new Point(-500, -100, 0), new Point(lengthToX, -100, 0), new Point(lengthToX, -110, -20), new Point(-500, -110, -20)).setEmission(new Color(GRAY))
+                .setMaterial(new Material().setKs(1).setKd(0.3).setKr(0.3).setShininess(19));
+        Color bowlingMain= new Color(WHITE).scale(2);
         BowlingPin bowlingPin1= new BowlingPin(new Ray(new Point(firstX,0,0),new Vector(0,0,1)),bowlMat,bowlingMain,new Color(RED));
         BowlingPin bowlingPin2= new BowlingPin(new Ray(new Point(firstX+distanceBetweenPins,-distanceBetweenPins/2,0),new Vector(0,0,1)),bowlMat,bowlingMain,new Color(RED));
         BowlingPin bowlingPin3= new BowlingPin(new Ray(new Point(firstX+distanceBetweenPins,distanceBetweenPins/2,0),new Vector(0,0,1)),bowlMat,bowlingMain,new Color(RED));
@@ -46,7 +64,8 @@ public class OurPictureMP1 {
         BowlingPin bowlingPin8= new BowlingPin(new Ray(new Point(firstX+3*distanceBetweenPins,-distanceBetweenPins/2,0),new Vector(0,0,1)),bowlMat,bowlingMain,new Color(RED));
         BowlingPin bowlingPin9= new BowlingPin(new Ray(new Point(firstX+3*distanceBetweenPins,distanceBetweenPins/2,0),new Vector(0,0,1)),bowlMat,bowlingMain,new Color(RED));
         BowlingPin bowlingPin10= new BowlingPin(new Ray(new Point(firstX+3*distanceBetweenPins,1.5*distanceBetweenPins,0),new Vector(0,0,1)),bowlMat,bowlingMain,new Color(RED));
-        _scene._geometries.add(bowlingPin1,lane,rightPolL,rightPolR,leftPolL,leftPolR,bowlingPin2,bowlingPin3,bowlingPin4,bowlingPin5,bowlingPin6,bowlingPin7,bowlingPin8,bowlingPin9,bowlingPin10);
+        //_scene._geometries.add(bowlingPin1,lane,rightPolL,rightPolR,leftPolL,leftPolR,bowlingPin2,bowlingPin3,bowlingPin4,bowlingPin5,bowlingPin6,bowlingPin7,bowlingPin8,bowlingPin9,bowlingPin10);
+        _scene._geometries.add(bowlingPin1,lane,rightPolL,rightPolR,leftPolL,leftPolR,bowlingPin2,bowlingPin3,bowlingPin4,bowlingPin5,bowlingPin6);
         Polygon laneRight = (Polygon) new Polygon(new Point(-500, -140, 0), new Point(lengthToX, -140, 0), new Point(lengthToX, -340, 0), new Point(-500, -340, 0)).setEmission(laneColor)
                 .setMaterial(new Material().setKs(1).setKd(0.6).setKr(0.3).setShininess(19));
         Polygon laneLeft = (Polygon) new Polygon(new Point(-500, 140, 0), new Point(lengthToX, 140, 0), new Point(lengthToX, 340, 0), new Point(-500, 340, 0)).setEmission(laneColor)
@@ -80,16 +99,16 @@ public class OurPictureMP1 {
 
 
 
-        Sphere lightCover= (Sphere)new Sphere(new Point(1500,100,100), 10d).setEmission(new Color(WHITE)) //
-                .setMaterial(new Material().setKd(0.4).setKs(0.2).setShininess(19).setKt(0.2));
-        _scene._geometries.add(lightCover);
-
-
-        PointLight middleLight= new PointLight(new Color(GRAY),new Point(1500,100,100));
-
-        _scene._lights.add(middleLight);
-
-
+//        Sphere lightCover= (Sphere)new Sphere(new Point(1500,100,100), 10d).setEmission(new Color(WHITE)) //
+//                .setMaterial(new Material().setKd(0.4).setKs(0.2).setShininess(19).setKt(0.3));
+//        _scene._geometries.add(lightCover);
+//
+//
+//        PointLight middleLight= new PointLight(new Color(GRAY),new Point(1500,100,100));
+////
+        //_scene._lights.add(middleLight);
+//
+//
 
 
 
@@ -97,10 +116,10 @@ public class OurPictureMP1 {
                 .setRayTracer(new RayTracerBasic(_scene)) //
                 .renderImage() //
                 .writeToImage();
-        cameraTop.setImageWriter(new ImageWriter("ourPictureMP1Up", 500, 500)) //
-                .setRayTracer(new RayTracerBasic(_scene)) //
-                .renderImage() //
-                .writeToImage();
+//        cameraTop.setImageWriter(new ImageWriter("ourPictureMP1Up", 500, 500)) //
+//                .setRayTracer(new RayTracerBasic(_scene)) //
+//                .renderImage() //
+//                .writeToImage();
 
     }
 }
